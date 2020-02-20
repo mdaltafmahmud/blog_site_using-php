d7ad6bd2dc7e4bc9bc3c4e5ec09a6c918b10dfbb
Altaf <altafmahmudmbpi1617@gmail.com>
2020-02-20 13:01:24 -0800

    title,body,tags,author,image updated

--- admin/addpost.php
+++ admin/addpost.php
@@ -5,8 +5,47 @@
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Add New Post</h2>
+
+        <?php 
+        if ($_SERVER ['REQUEST_METHOD']== 'POST') {
+           $title  = mysqli_real_escape_string($db->link, $_POST['title']);
+           $body   = mysqli_real_escape_string($db->link, $_POST['body' ]);
+           $tags   = mysqli_real_escape_string($db->link, $_POST['tags' ]);
+           $author = mysqli_real_escape_string($db->link, $_POST['author']);
+
+           $permited  = array('jpg', 'jpeg', 'png', 'gif');
+           $file_name = $_FILES['image']['name'];
+           $file_size = $_FILES['image']['size'];
+           $file_temp = $_FILES['image']['tmp_name'];
+       
+           $div = explode('.', $file_name);
+           $file_ext = strtolower(end($div));
+           $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+           $uploaded_image = "upload/images/".$unique_image;
+
+           if ($title =="" || $body =="" || $tags =="" || $author =="" || $file_name =="") {
+            echo "<span class='error'> Field Must Not Be Empty !!.</span>";
+           }elseif ($file_size >1048567) {
+            echo "<span class='error'>Image Size should be less then 1MB!</span>"; 
+
+           } elseif (in_array($file_ext, $permited) === false) {
+            echo "<span class='error'>You can upload only:-".implode(', ', $permited)."</span>";
+           } else{
+           move_uploaded_file($file_temp, $uploaded_image);
+           $query = "INSERT INTO tbl_post (cat,title,body,image,author,tags)VALUES('cat','$title','$body','$uploaded_image','$author','$tags')";
+           $inserted_rows = $db->insert($query);
+           if ($inserted_rows) {
+            echo "<span class='success'>Post Inserted Successfully.
+            </span>";
+           }else {
+            echo "<span class='error'>Post Not Inserted !</span>";
+           }
+        }
+    }
+        
+        ?>
         <div class="block">               
-            <form action="" method="" enctype="multipart/form-data">
+            <form action="" method="post" enctype="multipart/form-data">
             <table class="form">
                 
                 <tr>
@@ -14,7 +53,7 @@
                         <label>Title</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Enter Post Title..." class="medium" />
+                        <input type="text" name="title" placeholder="Enter Post Title.." class="medium"/>
                     </td>
                 </tr>
                 
@@ -24,7 +63,7 @@
                         <label>Category</label>
                     </td>
                     <td>
-       <select id="select" name="select">
+             <select id="select" name="select">
                  <option> Select Category  </option> 
                   <?php 
                 $query     ="SELECT * FROM catagory ";
@@ -34,8 +73,8 @@
                      ?>
                 <option value="<?php echo $result['id'];?>"><?php echo $result['name'];?></option>  
                  <?php   } }?>
-       </select>
-                    </td>
+          </select>
+                     </td>
                 </tr> <!--  options end  here  -->
                    
                 <tr> <!--  image upload start from below  -->
@@ -56,7 +95,23 @@
                     </td>
                 </tr> <!--  text editor/ body text upload end here  -->
 
-               
+                <tr> <!--  tags options start from below  -->
+                    <td>
+                        <label>tags</label>
+                    </td>
+                    <td>
+                        <input type="text" name="tags" placeholder="Enter your tags"></input>
+                    </td>
+                </tr> <!--  tags options  upload end here  -->  
+
+                <tr> <!--  tags options start from below  -->
+                    <td>
+                        <label>Author</label>
+                    </td>
+                    <td>
+                    <input type="text" name="author" placeholder="Enter Author Name"></input>
+                    </td>
+                </tr> <!--  tags options   end here  -->  
                 
 
                 <tr>
