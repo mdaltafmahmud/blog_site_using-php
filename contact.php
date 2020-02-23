43e1378a9f1ce8af3c3b551f655d32ee42ec0a5b
Altaf <altafmahmudmbpi1617@gmail.com>
2020-02-22 18:49:32 -0800

    contact file has been modified

--- contact.php
+++ contact.php
@@ -1,40 +1,92 @@
 <?php include 'inc/header.php'; ?>
 
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$firstname = $formate->validation($_POST['firstname']);
+	$lastname = $formate->validation($_POST['lastname']);
+	$email = $formate->validation($_POST['email']);
+	$body  = $formate->validation($_POST['body']);
+
+	$firstname = mysqli_real_escape_string($db->link,$firstname);
+	$lastname  = mysqli_real_escape_string($db->link,$lastname);
+	$email     = mysqli_real_escape_string($db->link,$email);
+	$body      = mysqli_real_escape_string($db->link,$body);
+
+	$error ="";
+	if (empty($firstname)) {
+		$error = "First name must not be empty !!";
+	}elseif (empty($lastname)) {
+		$error = "Last name must not be empty !!";
+	}elseif (empty($email)) {
+		$error = "Email must not be empty !!";
+	}elseif (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+		$error = "Inavalidate Email Address !!";
+	}elseif (empty($body)) {
+		$error = "Message Field must not be empty !!";
+	}
+	
+	else{
+		$query = "INSERT INTO tbl_contact (firstname,lastname,email,body)
+		VALUES('$firstname','$lastname','$email','$body')";
+		$inserted_rows = $db->insert($query);
+		if ($inserted_rows) {
+			$msg ="Message Sent Sucessfully";
+		}else {
+			$error = "Message  not sent !!";
+		}
+	}
+	
+}
+?>
+
+
+
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 			<div class="about">
 				<h2>Contact us</h2>
+
+				<?php 
+					if (isset($error)) {
+						echo "<span style='color:red'>$error</span>";
+					}
+					if (isset($msg)) {
+						echo "<span style='color:green'>$msg</span>";
+					}
+				
+				?>
 			<form action="" method="post">
 				<table>
 				<tr>
 					<td>Your First Name:</td>
 					<td>
-					<input type="text" name="firstname" placeholder="Enter first name" required="1"/>
+					<input type="text" name="firstname" placeholder="Enter first name"/>
 					</td>
 				</tr>
 				<tr>
 					<td>Your Last Name:</td>
 					<td>
-					<input type="text" name="lastname" placeholder="Enter Last name" required="1"/>
+					<input type="text" name="lastname" placeholder="Enter Last name"/>
 					</td>
 				</tr>
 				
 				<tr>
 					<td>Your Email Address:</td>
 					<td>
-					<input type="email" name="email" placeholder="Enter Email Address" required="1"/>
+					<input type="text" name="email" placeholder="Enter Email Address"/>
 					</td>
 				</tr>
 				<tr>
 					<td>Your Message:</td>
 					<td>
-					<textarea></textarea>
+					<textarea name="body"></textarea>
 					</td>
 				</tr>
 				<tr>
 					<td></td>
 					<td>
-					<input type="submit" name="submit" value="Submit"/>
+					<input type="submit" name="submit" value="Send"/>
 					</td>
 				</tr>
 		</table>
