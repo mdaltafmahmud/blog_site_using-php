c43e419e8962f39f24761f37104616960e2b251f
Altaf <altafmahmudmbpi1617@gmail.com>
2020-02-22 19:28:25 -0800

    inbox file has been modified

--- admin/inbox.php
+++ admin/inbox.php
@@ -9,55 +9,72 @@
 					<thead>
 						<tr>
 							<th>Serial No.</th>
+							<th>Name</th>
+							<th>Email</th>
 							<th>Message</th>
+							<th>Date</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php 
+					
+					$query    = "SELECT * FROM  tbl_contact WHERE status='0' ORDER BY id DESC ";
+					$msg = $db->select($query);
+					if ($msg) {
+					     $i=0;
+					while($result = $msg->fetch_assoc() ){
+					$i++;
+				?>
+						<tr class="odd gradeX">
+							<td><?php echo $i; ?></td>
+							<td><?php echo $result['firstname'].' '.$result['lastname'];?></td>
+							<td><?php echo $result['email'];?></td>
+							<td><?php echo $formate->textshorten($result['body'],30);?></td>
+							<td><?php echo $formate->formatDate($result['date']);?></td>
+							<td>
+							<a href="viewMsg.php?msgid=<?php echo $result['id'];?>"> View</a> ||
+							<a href="replayMsg.php?msgid=<?php echo $result['id'];?>"> replay</a>||
+							<a href="?seenid=<?php echo $result['id'];?>"> Seen</a> 
+							</td>
+						</tr>
+
+					<?php } }?>
+					</tbody>
+				</table>
+               </div>
+            </div>
+
+			<div class="box round first grid">
+                <h2>Seen </h2>
+                <div class="block">        
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Message</th>
+							<th>Date</th>
 							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr class="odd gradeX">
 							<td>01</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
+							<td>Name</td>
+							<td>Email</td>
+							<td>Message</td>
+							<td>Date</td>
+							<td><a href="">Delete</a> </td>
 						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
+
+				
 					</tbody>
 				</table>
                </div>
             </div>
+
         </div>
 
 
