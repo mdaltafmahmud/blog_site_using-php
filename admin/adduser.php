33f3c7543ca061eec2b4351c8f8d401743f81897
Altaf <altafmahmudmbpi1617@gmail.com>
2020-02-25 12:33:29 +0600

    some file has been modified

--- admin/adduser.php
+++ admin/adduser.php
@@ -4,64 +4,78 @@
 <?php include 'incAdmin/sidebar.php'; ?>
 <?php
     if (!Session::get('userRole')=='0') { 
-        // header("Location:index.php");
         echo "<script defer> location.href='index.php';</script>";
-      
     }
-    
  ?>
-       
-
     <div class="grid_10">
         <div class="box round first grid">
             <h2>Add New User</h2>
         <div class="block copyblock"> 
-               
+
      <!-- user create query  -->
     <?php 
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name     = $formate->validation($_POST['name']);
         $username = $formate->validation($_POST['username']);
         $password = $formate->validation(md5($_POST['password']));
-        $role     = (int)$formate->validation($_POST['role']);
-        // var_dump($role);
-        // var_dump($username);
-        // var_dump($password);
-        // die();
+        $email    = $formate->validation($_POST['email']);
+        $role     = $formate->validation($_POST['role']);
 
+        $name     = mysqli_real_escape_string($db->link,$name);
         $username = mysqli_real_escape_string($db->link,$username);
         $password = mysqli_real_escape_string($db->link,$password);
+        $email    = mysqli_real_escape_string($db->link,$email);
         $role     = mysqli_real_escape_string($db->link,$role);
 
-    if ($username == "" || $password == '' || $role == '') {
-        echo "<span class='error'>Field Must Not Be Empty !!.</span>";
-        }else{
-        $query      ="INSERT INTO user_tbl (username,password,role) VALUES('$username','$password',$role)";
-        $userInsert =$db->insert($query);
-        if ( $userInsert) {
-        echo "<span class='success'>User Created successfully !!.</span>";
-        }else{
-        echo "<span class='error'> User Not Created !!.</span>";
+        if ( $name == " " ||$username == " " || $password == ' ' || $email == ' ' || $role == ' ') {
+            echo "<span class='error'>Field Must Not Be Empty !!.</span>";
         }
-        
-    }
-}
+        else{
+            $emailquery ="SELECT * FROM user_tbl WHERE email= '$email' LIMIT 1 ";
+            $mailcheck  = $db->select($emailquery);
+            if($mailcheck != false){
+                echo "<span class='error'> Email already exists !!.</span>";
+            } 
+            else{
+                $query    ="INSERT INTO user_tbl (name,username, password, email, role) 
+                VALUES('$name','$username','$password','$email', '$role')";
+
+                $userInsert = $db->insert($query);
+                if ($userInsert) {
+                    echo "<span class='success'>User Created successfully !!.</span>";
+                }else{
+                echo "<span class='error'> User Not Created !!.</span>";
+                } 
+            }
+        }
+  }
 
 ?>
-
-
         <form action="adduser.php" method="post" id="sentmsg" >
-        <table class="form">					
+        <table class="form">
+        <tr>
+                <td><label for=""> Name</label></td>
+                <td>
+                    <input type="text" name="name" placeholder="Enter name"/>
+                </td>
+            </tr>					
             <tr>
-                 <td><label for="">User Name</label></td>
+                <td><label for="">User Name</label></td>
                 <td>
                     <input type="text" name="username" placeholder="Enter  Username"/>
                 </td>
             </tr>
             <tr>
-                 <td><label for="">Password</label></td>
+                <td><label for="">Email</label></td>
                 <td>
-                    <input type="text" name="password" placeholder="Enter  Password"/>
+                    <input type="email" name="email" placeholder="Enter a Valid Email Address"/>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="">Password</label></td>
+                <td>
+                    <input type="password" name="password" placeholder="Enter  Password"/>
                 </td>
             </tr>
             <tr>
@@ -75,7 +89,6 @@
                   </select>
                 </td>
             </tr>
-
             <td></td>
             <tr> 
                 <td>
@@ -85,11 +98,8 @@
 
         </table>
         </form>
-           
-
-                </div>
-            </div>
         </div>
-
+    </div>
+</div>
 <?php include 'incAdmin/footer.php';?> 
  
--- admin/login.php
+++ admin/login.php
@@ -30,17 +30,14 @@
 		$query  = "SELECT * FROM user_tbl WHERE username = '$username' AND password = '$password' ";
 		$result = $db->select($query);
 		if ($result !=false) {
-			$value = mysqli_fetch_array($result);
-			$row   = mysqli_num_rows($result);
-			if ($row > 0) {
+			//$value = mysqli_fetch_array($result);
+			    $value = $result->fetch_assoc();
 				Session::set("login",true);
 				Session::set("username", $value['username']);
 				Session::set("userId", $value['id']);
 				Session::set("userRole", $value['role']);
 				header("Location:index.php");
-			}else{
-				echo "<span style='color:red;font-size:18px;'>No Result Found..!!</span>";
-			}
+		
 		}else{
 			echo "<span style='color:red;font-size:18px;'>Username Or Password Not Match ..!!</span>";
 		}
